95c5998a58df4a4de1ba49b9818fd01b702d26e2
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-12-09 12:50:35 -0500

    refactor: removing the last bit of mutation, replacing if/else with ? :

--- src/day1/sum-captcha.js
+++ src/day1/sum-captcha.js
@@ -4,26 +4,19 @@ const sumCaptcha = (fetchNextElement, captcha) => {
   // '1122' => ['1', '1', '2', '2'] => [1, 1, 2, 2]
   const characters = [...captcha].map(Number);
 
-  let sum = 0;
-  characters.forEach((currentCharacter, i, allCharacters) => {
-    sum += addIfEqual(currentCharacter, fetchNextElement(allCharacters, i));
-  });
-
-  return sum;
+  return R.sum(
+    characters.map((currentCharacter, i) => {
+      return addIfEqual(currentCharacter, fetchNextElement(characters, i));
+    })
+  );
 };
 
 const addIfEqual = (int1, int2) => {
-  if (int1 === int2) {
-    return int1;
-  }
-  return 0;
+  return int1 === int2 ? int1 : 0;
 };
 
 const getNextElement = (array, index) => {
-  if (isLastElement(array, index)) {
-    return array[0];
-  }
-  return array[index + 1];
+  return isLastElement(array, index) ? array[0] : array[index + 1];
 };
 
 const isLastElement = (array, index) => {
@@ -32,11 +25,9 @@ const isLastElement = (array, index) => {
 
 const getHalfwayElement = (array, index) => {
   const halfArrayLength = array.length / 2;
-  if (index >= halfArrayLength) {
-    return array[index - halfArrayLength];
-  }
-
-  return array[index + halfArrayLength];
+  return index >= halfArrayLength
+    ? array[index - halfArrayLength]
+    : array[index + halfArrayLength];
 };
 
 const sumCaptchaByNextChar = R.curry(sumCaptcha)(getNextElement);
