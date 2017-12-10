a4b1a3a766d801960e7b68a6976afb6552dd2173
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-12-09 19:48:03 -0500

    feat: first pass at part 2

--- src/day2/checksum.js
+++ src/day2/checksum.js
@@ -1,19 +1,43 @@
 const R = require('ramda');
 
 const checksum = data => {
-  return R.sum(data.map(findDifferenceForRow));
+  return R.sum(R.map(findDifferenceForRow, data));
+};
+
+const divisionChecksum = data => {
+  return R.sum(R.map(findEvenDivisionNumberForRow, data));
 };
 
 const findDifferenceForRow = row => {
   return R.subtract(findMax(row))(findMin(row));
 };
 
-const findMax = list => {
-  return R.reduce(R.max, -Infinity, list);
+const findEvenDivisionNumberForRow = row => {
+  return R.reduce(
+    (prev, number) => {
+      return checkEvenDivisionForNumberAgainstRow(number, row) === 0
+        ? prev
+        : checkEvenDivisionForNumberAgainstRow(number, row);
+    },
+    0,
+    row
+  );
 };
 
-const findMin = list => {
-  return R.reduce(R.min, Infinity, list);
+const checkEvenDivisionForNumberAgainstRow = (number, list) => {
+  return R.reduce(
+    (prev, current) => {
+      return number !== current && number % current === 0
+        ? R.divide(number, current)
+        : prev;
+    },
+    0,
+    list
+  );
 };
 
-module.exports = checksum;
+const findMax = R.reduce(R.max, -Infinity);
+
+const findMin = R.reduce(R.min, Infinity);
+
+module.exports = {checksum, divisionChecksum};
